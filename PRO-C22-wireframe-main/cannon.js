4158404a4235f94a504823ecf696a1253a036c0b
rhnvenu <109427183+rhnvenu@users.noreply.github.com>
2022-08-24 11:12:40 -0400

    Add files via upload

---
+++ PRO-C22-wireframe-main/cannon.js
@@ -0,0 +1,70 @@
+class Cannon {
+constructor(x,y,width,height,angle){
+this.x=x
+this.y=y
+this.width=width
+this.height=height
+this.angle=angle
+this.cannonImage=loadImage("assets/canon.png")
+this.cannonbase=loadImage("assets/cannonBase.png")
+}
+
+display(){
+
+
+
+    push ()
+    imageMode(CENTER)
+    image (this.cannonImage,this.x,this.y,this.width,this.height)
+    pop ()
+    image(this.cannonbase,70,20,200,200)
+    noFill()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
